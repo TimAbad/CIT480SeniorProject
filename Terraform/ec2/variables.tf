c6dae952b4aa4cd074e1893efdfd26817a8e690e
Kristina Utas <kristinautas@kristinasmac.lan>
2021-11-17 00:21:18 -0800

    created Terraform folder structure, created reusable modules and variables, as well as 2 ec2 instances (1 per each public subnet). Updated the code to be compatible with the newest version of Terraform. Opened ports 9100, 9090, and 3000. To test, place your public key in /tmp folder and run terraform commands in the root directory

---
+++ Terraform/ec2/variables.tf
@@ -0,0 +1,11 @@
+variable "my_public_key" { 
+}
+
+variable "instance_type" {
+}
+
+variable "security_group" {
+}
+
+variable "subnets" {
+}
---
+++ Terraform/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "vpc" {
+  source        = "./vpc"
+  vpc_cidr      = "10.0.0.0/16"
+  public_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+module "ec2" {
+  source         = "./ec2"
+  my_public_key  = "/tmp/id_rsa.pub"
+  instance_type  = "t2.micro"
+  security_group = module.vpc.security_group
+  subnets        = module.vpc.subnets
+}
