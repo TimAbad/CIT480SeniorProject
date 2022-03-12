6945c39bf981a78d3302fa390a3a33cc53d21eff
Kristina Utas <kristina.utas.94@my.cdun.edu>
2022-03-12 04:39:14 +0000

    added private subnets to vpc module outputs

--- Terraform/vpc/outputs.tf
+++ Terraform/vpc/outputs.tf
@@ -22,3 +22,10 @@ output "subnet2" {
   value = "${element(aws_subnet.public_subnet.*.id, 2 )}"
 }
 
+output "private_subnet1" {
+  value = "${element(aws_subnet.private_subnet.*.id, 1 )}"
+}
+
+output "private_subnet2" {
+  value = "${element(aws_subnet.private_subnet.*.id, 2 )}"
+}
