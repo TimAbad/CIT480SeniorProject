05b67b2bcaa1e60e7984b912ea06d76350035f96
Kristina Utas <kristina.utas@gmail.com>
2022-05-02 17:50:28 -0700

    added iam password policy

--- Terraform/iam/main.tf
+++ Terraform/iam/main.tf
@@ -33,3 +33,12 @@ resource "aws_iam_user_login_profile" "ieprofile" {
 	password_reset_required = true
 	pgp_key = "keybase:cit481"
 }
+
+resource "aws_iam_account_password_policy" "strict" {
+        minimum_password_length = 7
+        require_lowercase_characters = true
+        require_numbers = true
+        require_uppercase_characters = true
+        require_symbols = true
+        allow_users_to_change_password = true
+}
